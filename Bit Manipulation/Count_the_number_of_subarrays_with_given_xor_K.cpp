c933c42de84c45e704aa13729418eb8da2f1d03c
Sapna Kul <91309280+Sapna127@users.noreply.github.com>
2022-06-04 15:35:51 +0530

    Create Count_the_number_of_subarrays_with_given_xor_K.cpp

---
+++ Bit Manipulation/Count_the_number_of_subarrays_with_given_xor_K.cpp
@@ -0,0 +1,6 @@
+//Given an array of integers A and an integer B. Find the total number of subarrays having bitwise XOR of all elements equal to B.
+//https://takeuforward.org/data-structure/count-the-number-of-subarrays-with-given-xor-k/
+
+int solve(vector<int> &v, int k){
+     unordered_map<int,int> mp; 
+}
