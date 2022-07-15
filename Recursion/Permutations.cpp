f9d609309ddf2c1bf4d247f8016f13666a22602d
Sapna Kul <91309280+Sapna127@users.noreply.github.com>
2022-07-15 13:17:10 +0530

    Create Permutations.cpp

---
+++ Recursion/Permutations.cpp
@@ -0,0 +1,18 @@
+void helper(int index, vector<int> &nums, vector<vector<int>> &ans) {
+        if(index == nums.size()) {
+            ans.push_back(nums); 
+            return; 
+        }
+        for(int i = index;i<nums.size();i++) {
+            swap(nums[index], nums[i]); 
+            helper(index+1, nums, ans); 
+            swap(nums[index], nums[i]); 
+        }
+        
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans; 
+        helper(0, nums, ans); 
+        return ans; 
+    }
