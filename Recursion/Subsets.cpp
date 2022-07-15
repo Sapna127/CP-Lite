3cfed405037a6aa462ec8610832928d1915621dd
Sapna Kul <91309280+Sapna127@users.noreply.github.com>
2022-07-15 13:21:46 +0530

    Update Subsets.cpp

--- Recursion/Subsets.cpp
+++ Recursion/Subsets.cpp
@@ -1,1 +1,22 @@
-
+ void subset_rec(vector<int>&nums, int idx,vector<vector<int>>&result){
+        if(idx==nums.size()-1){
+            result.push_back({});
+            result.push_back({nums[idx]});
+        }
+        else{
+            subset_rec(nums,idx+1,result);
+            int n=result.size();
+            for(int i=0;i<n;i++){
+                vector<int> r = result[i];
+                r.push_back(nums[idx]);
+                result.push_back(r);
+            }
+        }
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        if(nums.empty()) return {{}};
+        vector<vector<int>> result;
+        subset_rec(nums,0,result);
+        return result;
+    }
